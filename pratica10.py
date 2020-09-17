56efeb1d39c7388ac63e019e3e5f2930eea43441
Lorena <lorena_dasilvaelias@hotmail.com>
2020-09-16 21:05:17 -0300

    modifiquei opcao de distancia

--- pratica10.py
+++ pratica10.py
@@ -25,16 +25,13 @@ def distanciaEuclideana(instancia1, instancia2, dimensao):
         distancia += pow(instancia1[x] - instancia2[x], 2)
     return round(math.sqrt(distancia), 4)
 
-def calculaDistancia(X, escolhaDistancia):
+def calculaDistancia(X):
     distancias = []
     distancia = []
     for x in range(len(X)):
         for y in range(len(X)):
             dimensao = len(X[x])
-            if (escolhaDistancia == '0'):
-                dist = distanciaEuclideana(X[x], X[y], dimensao)
-            elif (escolhaDistancia == '1'):
-                dist = distanciaManhattan(X[x], X[y], dimensao)
+            dist = distanciaEuclideana(X[x], X[y], dimensao)
             distancia.append(dist)
         distancias.append(distancia)
         distancia = []
@@ -56,11 +53,5 @@ df = pd.read_csv(arquivoorig)
 X = df.loc[:, df.columns != df.columns[-1]].values
 y = df.loc[:, df.columns[-1]].values
 
-print("---------------------------------------------------------------------------")
-print('Escolha a distancia a ser empregada')
-print('0   -       Distancia Euclideana')
-print('1   -       Distancia de Manhattan\n')
-escolha = input('Escolha:  ')
-print("---------------------------------------------------------------------------")
-distancias = calculaDistancia(X, escolha)
+distancias = calculaDistancia(X)
 writeToFile(distancias, arquivodest)
